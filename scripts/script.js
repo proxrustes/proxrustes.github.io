b39f7cc6364b282c0c8572d87ba64c5e05e39862
proxrustes <proxrustes@gmail.com>
2024-01-17 11:23:49 +0100

    + populate functions

--- scripts/script.js
+++ scripts/script.js
@@ -18,11 +18,37 @@ const Work = [
   { title: "name2", time: "description1", tasks: ["1", "1"] },
   { title: "name3", time: "description1", tasks: ["1", "1"] }
 ];
+
 function createPost(post) {
   return `
       <div class="post">
           <h1>${post.title}</h1>
           <p>${post.description}</p>
+      </div>
+  `;
+}
+function createSkill(skill) {
+  return `
+      <div class="skill">
+          <h1>${skill.title}</h1>
+          <p>${skill.iconURL}</p>
+      </div>
+  `;
+}
+
+function createWorkExp(work) {
+  return `
+      <div class="work">
+          <h1>${work.title}</h1>
+          <p>${work.time}</p>
+      </div>
+  `;
+}
+function createEdu(edu) {
+  return `
+      <div class="skill">
+          <h1>${edu.title}</h1>
+          <p>${edu.time}</p>
       </div>
   `;
 }
@@ -50,18 +76,43 @@ function showTargetContainer(targetId) {
   const targetContainer = document.getElementById(targetId);
   targetContainer.style.display = "block";
 }
+function populateEdu() {
+  const Container = document.getElementById("education-container");
 
+  Work.forEach((work) => {
+    const Html = createEdu(work);
+    Container.insertAdjacentHTML("beforeend", Html);
+  });
+}
+function populateSkills() {
+  const Container = document.getElementById("skills-container");
+
+  Work.forEach((work) => {
+    const Html = createSkill(work);
+    Container.insertAdjacentHTML("beforeend", Html);
+  });
+}
+function populateExp() {
+  const Container = document.getElementById("experience-container");
+
+  Work.forEach((work) => {
+    const Html = createWorkExp(work);
+    Container.insertAdjacentHTML("beforeend", Html);
+  });
+}
 function populatePosts() {
-  const postContainer = document.getElementById("miscellaneous-container");
+  const Container = document.getElementById("miscellaneous-container");
 
   Posts.forEach((post) => {
-    const postHtml = createPost(post);
-    console.log(postHtml);
-    postContainer.insertAdjacentHTML("beforeend", postHtml);
+    const Html =  createPost(post);
+    Container.insertAdjacentHTML("beforeend", Html);
   });
 }
 
 document.addEventListener("DOMContentLoaded", function () {
   populatePosts();
+  populateEdu();
+  populateExp();
+  populateSkills();
   initTabs();
 });
